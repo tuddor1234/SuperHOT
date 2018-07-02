cdb98b5001048907bd7e8243fee2ecf59df11ee7
tuddor1234 <tuddor1234@yahoo.com>
2018-07-02 17:39:21 +0300

    Added Player And Camera Movement

---
+++ SuperHot/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MovePlayer : MonoBehaviour {
+
+    public float speed = 100f;
+
+
+    private float h;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        if(rb==null)
+        {
+            Debug.LogError("Player has no rigidbody");
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        //Input
+        h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        Move(h);
+        
+
+
+	}
+
+    void Move(float h)
+    {
+        movement.Set(h, 0,0);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(this.transform.position + movement);
+    }
+
+}
